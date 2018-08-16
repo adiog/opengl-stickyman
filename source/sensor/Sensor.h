06b8523dc4415ed7dc1f1980347babd95e528fcc
adiog <adiog@brainfuck.pl>
2018-08-16 07:41:19 +0200

    concurrency

--- source/sensor/Sensor.h
+++ source/sensor/Sensor.h
@@ -4,9 +4,11 @@
 #pragma once
 
 #include <memory>
+#include <mutex>
 #include <bio/Segment.h>
 #include <fusion/Data.h>
 #include <fusion/Fusion.h>
+#include <fusion/Kalman.h>
 #include <sensor/Calibrator.h>
 #include <sensor/Data.h>
 
@@ -15,19 +17,34 @@ namespace sensor {
 class Sensor
 {
 public:
+    Sensor(bio::Segment segment)
+            : segment(segment)
+            , aFusion(std::make_unique<fusion::Kalman>())
+    {
+    }
+
     void calibrate(CalibrationStep calibrationStep)
     {
+        std::unique_lock<std::mutex> sensorLock{sensorMutex};
         calibrator.calibrate(rawData.accelerometer, calibrationStep);
     }
 
     void update(Data rawData)
     {
+        std::unique_lock<std::mutex> sensorLock{sensorMutex};
         this->rawData = rawData;
         this->data = calibrator.transform(rawData);
         fusionData = aFusion->apply(this->data);
     }
 
+    fusion::Data get()
+    {
+        std::unique_lock<std::mutex> sensorLock{sensorMutex};
+        return fusionData;
+    }
+
 private:
+    std::mutex sensorMutex;
     bio::Segment segment;
     sensor::Data rawData;
     sensor::Data data;
